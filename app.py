44a021c83c1b5cab01d91a91a8377b3848e2c76b
shorbagy <mohamedelshorbagy96@gmail.com>
2019-04-24 15:46:05 +0200

    add comment guidelines

--- app.py
+++ app.py
@@ -6,10 +6,24 @@ from flask_restplus import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+"""
+    @method GET
+    @description hello endpoint for testing
+    @param {Dict} self
+    @returns {JSON}
+"""
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+"""
+    @method POST
+    @desc Login Endpoint for login by username
+    @param {string} username
+    @body { <name:string>, <age:int>, <data:{}> }
+    @returns {JSON} 
+"""
 
 @api.route('/login/<username>')
 class Login(Resource):
