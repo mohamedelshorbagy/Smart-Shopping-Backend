d6bfb72bb2100d1b7fa07cbc0d8c78a3cd1247a6
shorbagy <mohamedelshorbagy96@gmail.com>
2019-05-04 18:20:12 +0200

    add query to app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_cors import CORS
 import os
 from preprocess import preprocess_image
 from werkzeug.utils import secure_filename
+from DB import readByCode, readByProductName
 
 app = Flask(__name__)
 # Enable CORS to avoid cross-origin problem so can web and mobile application use it  
@@ -41,10 +42,20 @@ class Upload(Resource):
         image_name = 'images/{0}_{1}'.format(os.getpid(),secure_filename(f.filename))
         f.save(image_name)
         data = preprocess_image(image_name)
+        rData = []
+        if data['code'] is not None:
+            result = readByCode(data['code'])
+            if bool(result) == True:
+                rData.append(result) 
+        else:
+            result = readByProductName(data['ocr'])
+            if len(result) >= 1:
+                rData = result
+
         # TODO:
         # 1. Make a SQL Query to Database to get the Item or related Items
         # 2. Return the Result to the Mobile
-        return jsonify(data)
+        return jsonify({ 'AlgoResult': data, 'data': rData })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
